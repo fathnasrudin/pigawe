0df2ad8b96f2513535ba7f206fa75fcd9878221a
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-14 18:42:05 +0700

    Add basic funtional todolist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,112 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+type ITask = {
+  id: string;
+  title: string;
+  status: "done" | "todo";
+};
+
+const generateId = () => crypto.randomUUID();
+
+const tasksData: ITask[] = [
+  {
+    id: generateId(),
+    title: "Makan siang",
+    status: "todo",
+  },
+  {
+    id: generateId(),
+    title: "Makan malam",
+    status: "todo",
+  },
+];
+
+const initialTaskDraft = { title: "" };
 
 export default function Home() {
+  const [tasks, setTasks] = useState(() => tasksData);
+  const [taskDraft, setTaskDraft] = useState(() => initialTaskDraft);
+
+  function handleToggleTaskStatus(taskId: string) {
+    const newTasks = tasks.map((t) => {
+      if (t.id === taskId) {
+        t.status = t.status === "todo" ? "done" : "todo";
+      }
+      return t;
+    });
+    setTasks(newTasks);
+  }
+
+  function handleAddTask({ title }: { title: string }) {
+    const newTask: ITask = {
+      id: generateId(),
+      title,
+      status: "todo",
+    };
+    setTasks([newTask, ...tasks]);
+    setTaskDraft(initialTaskDraft);
+  }
+
+  function handleDeleteTask(taskId: string) {
+    const newTasks = tasks.filter((t) => t.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="max-w-md mx-auto p-4 flex flex-col gap-6">
+      {/* Add Task Form */}
+      <div className="p-4 border rounded-sm flex gap-4">
+        <input
+          className="flex-1 border-b-2"
+          type="text"
+          placeholder="your task"
+          value={taskDraft.title}
+          onChange={(e) => {
+            setTaskDraft({ ...taskDraft, title: e.target.value });
+          }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Button
+          size={"sm"}
+          variant={"default"}
+          onClick={(e) => {
+            e.preventDefault();
+            handleAddTask(taskDraft);
+          }}
+        >
+          A Button
+        </Button>
+      </div>
+
+      {/* task list */}
+      <div className="flex flex-col gap-1">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="border rounded-sm p-1 flex items-center gap-2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            <input
+              type="checkbox"
+              checked={task.status === "done"}
+              onChange={() => handleToggleTaskStatus(task.id)}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+            <div
+              className={`
+                flex-1
+                ${task.status === "done" && "line-through"}
+              `}
+            >
+              {task.title}
+            </div>
+            <Button size={"icon"} onClick={() => handleDeleteTask(task.id)}>
+              Del
+            </Button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
