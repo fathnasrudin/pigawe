df1da211ba49d142f305a5b187213c46433dab5d
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-15 13:54:43 +0700

    Add signup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,3 @@
+import { SignUpPage } from "@/features/auth/components/signup.page";
+
+export default SignUpPage;
---
+++ src/features/auth/components/signup-form.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+
+const initialDraft = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+  name: "",
+};
+
+export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const [draft, setDraft] = useState(initialDraft);
+
+  async function handleSubmit() {
+    console.log({ draft });
+    const result = await authClient.signUp.email(
+      {
+        email: draft.email, // user email address
+        password: draft.password, // user password -> min 8 characters by default
+        name: draft.name, // user display name
+        // image, // User image URL (optional)
+        // callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: (ctx) => {
+          //show loading
+          // return <p>loading...</p>
+          console.log("onRequest");
+        },
+        onSuccess: (ctx) => {
+          //redirect to the dashboard or sign in page
+          console.log("onSuccess");
+        },
+        onError: (ctx) => {
+          // display the error message
+          console.log(ctx.error);
+          alert(ctx.error.message);
+        },
+      },
+    );
+    console.log({ result });
+  }
+
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your information below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <FieldGroup>
+            {/* Full Name */}
+            <Field>
+              <FieldLabel htmlFor="name">Full Name</FieldLabel>
+              <Input
+                id="name"
+                type="text"
+                placeholder="John Doe"
+                required
+                value={draft.name}
+                onChange={(e) => setDraft({ ...draft, name: e.target.value })}
+              />
+            </Field>
+
+            {/* Email */}
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                value={draft.email}
+                onChange={(e) => setDraft({ ...draft, email: e.target.value })}
+              />
+              <FieldDescription>
+                We&apos;ll use this to contact you. We will not share your email
+                with anyone else.
+              </FieldDescription>
+            </Field>
+
+            {/* Password */}
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <Input
+                id="password"
+                type="password"
+                required
+                value={draft.password}
+                onChange={(e) =>
+                  setDraft({ ...draft, password: e.target.value })
+                }
+              />
+              <FieldDescription>
+                Must be at least 8 characters long.
+              </FieldDescription>
+            </Field>
+
+            {/* Confirm Password */}
+            <Field>
+              <FieldLabel htmlFor="confirm-password">
+                Confirm Password
+              </FieldLabel>
+              <Input
+                id="confirm-password"
+                type="password"
+                required
+                value={draft.confirmPassword}
+                onChange={(e) =>
+                  setDraft({ ...draft, confirmPassword: e.target.value })
+                }
+              />
+              <FieldDescription>Please confirm your password.</FieldDescription>
+            </Field>
+
+            <FieldGroup>
+              <Field>
+                <Button type="submit">Create Account</Button>
+
+                <Button variant="outline" type="button">
+                  Sign up with Google
+                </Button>
+
+                <FieldDescription className="px-6 text-center">
+                  Already have an account? <a href="#">Sign in</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
