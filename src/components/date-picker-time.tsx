411ac7baf837f63cd10388a8d279cdd9001e08eb
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-20 19:11:53 +0700

    feat(task-duedate): Add date and time picker component

---
+++ src/components/date-picker-time.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import * as React from "react";
+import { format } from "date-fns";
+import { ChevronDownIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function DatePickerTime({
+  date,
+  setDate,
+}: {
+  date: Date | undefined;
+  setDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
+}) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <FieldGroup className="mx-auto max-w-xs flex-row">
+      <Field>
+        <FieldLabel htmlFor="date-picker-optional">Date</FieldLabel>
+        <Popover open={open} onOpenChange={setOpen}>
+          {/* trigger */}
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date-picker-optional"
+              className="w-32 justify-between font-normal"
+            >
+              {date ? format(date, "PPP") : "Select date"}
+              <ChevronDownIcon data-icon="inline-end" />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={date}
+              captionLayout="dropdown"
+              defaultMonth={date}
+              onSelect={(date) => {
+                setDate(date);
+                setOpen(false);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </Field>
+      <Field className="w-32">
+        <FieldLabel htmlFor="time-picker-optional">Time</FieldLabel>
+        <Input
+          type="time"
+          id="time-picker-optional"
+          step="1"
+          defaultValue={date && "10:30:00"}
+          className="appearance-none bg-background [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+        />
+      </Field>
+    </FieldGroup>
+  );
+}
