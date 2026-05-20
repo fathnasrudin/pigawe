19bf9ed31fa21e84ef8e8271ec8daff772bcc515
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-20 07:00:07 +0700

    feat(task): Add task list loading skeleton

--- src/features/task/components/task-list.client.tsx
+++ src/features/task/components/task-list.client.tsx
@@ -1,11 +1,22 @@
 "use client";
+import { Skeleton } from "@/components/ui/skeleton";
 import { TaskItem } from "./task-item";
 import { useFetchTasks } from "./task.hook";
+
+export function TaskListSkeleton() {
+  return (
+    <div className="flex flex-col gap-1">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <Skeleton key={i} className="h-8 w-full rounded-sm"></Skeleton>
+      ))}
+    </div>
+  );
+}
 
 export function TaskListClient() {
   const { data: tasks, isLoading, error } = useFetchTasks();
 
-  if (isLoading) return <p>loading...</p>;
+  if (isLoading) return <TaskListSkeleton />;
   if (error) return <p>{error.message}</p>;
   if (!tasks) return <p>Data Not Found</p>;
   return (
