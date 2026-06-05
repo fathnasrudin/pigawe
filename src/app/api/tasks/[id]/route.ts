8922e99822cd87e61246c465e51fe3e5673513ab
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-06-05 19:56:55 +0700

    refactor(task): Move task route handler from app to task module

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,32 +1,4 @@
-import { Task, updateTaskInputSchema } from "@/features/task/task.schema";
-import { deleteTask, updateTask } from "@/features/task/task.service";
-import { badApiResponse, goodApiResponse } from "@/lib/next-api-response";
-
-export async function DELETE(
-  _req: Request,
-  { params }: { params: Promise<{ id: Task["id"] }> },
-) {
-  try {
-    const id = (await params).id;
-    await deleteTask(id);
-    return goodApiResponse({});
-  } catch (error) {
-    return badApiResponse(error);
-  }
-}
-
-export async function PUT(
-  request: Request,
-  { params }: { params: Promise<{ id: Task["id"] }> },
-) {
-  try {
-    const id = (await params).id;
-    const body = await request.json();
-    const data = updateTaskInputSchema.parse(body);
-
-    await updateTask(id, data);
-    return goodApiResponse({});
-  } catch (error) {
-    return badApiResponse(error);
-  }
-}
+export {
+  deleteTaskByIdController as DELETE,
+  updateTaskByIdController as PUT,
+} from "@/modules/task/task.controller";
