16be02fb8c86a06042417fa0934d6a1705effb98
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-06-08 14:10:44 +0700

    feat(error): Handle UI project not found

--- src/app/app/project/[id]/page.tsx
+++ src/app/app/project/[id]/page.tsx
@@ -5,6 +5,27 @@ import { TaskListWithFetch } from "@/modules/task/components/task-list";
 import { useFetchTasks } from "@/modules/task/components/task.hook";
 import { useFetchProjectById } from "@/modules/project/project.hook";
 import { use } from "react";
+import { ClientFetchError } from "@/lib/error/client-error";
+import {
+  Empty,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { FrownIcon } from "lucide-react";
+
+function ProjectNotFound() {
+  return (
+    <Empty className="text-muted-foreground">
+      <EmptyHeader>
+        <EmptyMedia>
+          <FrownIcon />
+        </EmptyMedia>
+        <EmptyTitle>Project Not Found</EmptyTitle>
+      </EmptyHeader>
+    </Empty>
+  );
+}
 
 export default function TasksPage({
   params,
@@ -12,8 +33,14 @@ export default function TasksPage({
   params: Promise<{ id: string }>;
 }) {
   const { id: projectId } = use(params);
-  const { isLoading, data: project } = useFetchProjectById(projectId);
+  const { isLoading, data: project, error } = useFetchProjectById(projectId);
   const fetchTasks = useFetchTasks({ searchParams: { projectId } });
+
+  if (error) {
+    if (error instanceof ClientFetchError && error.code === "NOT_FOUND") {
+      return <ProjectNotFound />;
+    }
+  }
 
   return (
     <div className="w-full">
